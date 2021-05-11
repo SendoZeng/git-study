1c32a5f6dea0ed814e64ca16afb0a87f88c3c902
Sendo <zeng.xiantao@tsimage.cn>
2021-05-11 17:54:51 +0800

    调用"doPlus()"方法实现加法

--- hello.java
+++ hello.java
@@ -1,9 +1,13 @@
 public class hello
 {
 	public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
+		int a = 1;
+		int b = 2;
+		int result = doPlus(a,b);
+		System.out.println(a+'+'+b+'='+result);
+
 
 	}
+	public static int doPlus(int a,int b){
+		return a+b;
 }
